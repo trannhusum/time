debea4b63bf2c4158823b297bb27c14fe9687b1f
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-07 16:35:34 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+            Console.ReadKey();
+        }
+    }
+}
